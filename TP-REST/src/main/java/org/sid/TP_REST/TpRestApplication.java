5a8f099ac4b1e0b375cb50ead2c4281d407574ba
OumaimaETTOURI <165429701+OumaimaETTOURI@users.noreply.github.com>
2024-12-09 10:26:46 +0100

    REST

---
+++ TP-REST/src/main/java/org/sid/TP_REST/TpRestApplication.java
@@ -0,0 +1,13 @@
+package org.sid.TP_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpRestApplication.class, args);
+	}
+
+}
---
+++ TP-REST/src/test/java/org/sid/TP_REST/TpRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.TP_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
